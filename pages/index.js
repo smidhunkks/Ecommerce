e60d0d6485dc7f1115951b9c6e06ab01c9df6746
Midhunkrishna k s <50834905+smidhunkks@users.noreply.github.com>
2022-07-16 23:06:58 +0530

    home page done

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner heroBanner={bannerData.length&&bannerData[0]} />
-      
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
       <div className="products-heading">
-        <h2>Best seller</h2>
-        <p>Speakers</p>
+        <h2>Best Seller Products</h2>
+        <p>Electronics</p>
       </div>
       <div className="products-container">
-        {products.map((product) => {
-          return product;
-        })}
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData&&bannerData[0]} />
     </>
   );
 };
