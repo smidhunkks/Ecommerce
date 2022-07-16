f4de65740f3cf4f4698eda42be442bdd0f5a004e
Midhunkrishna k s <50834905+smidhunkks@users.noreply.github.com>
2022-07-16 23:11:30 +0530

    home page done

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -19,7 +19,7 @@ const HeroBanner = ({ heroBanner }) => {
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
-            <h3>Description</h3>
+            <h5>Description</h5>
 
             <p>{heroBanner.desc}</p>
           </div>
